8deced456155a1a00e410f4aceb6a81bb45fdce0
akmorningstar <akash22048@iiitd.ac.in>
2025-01-12 16:45:29 +0530

    NavBar created

--- src/Components/Template.jsx
+++ src/Components/Template.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Footer from './Footer.jsx/Footer'
+import Footer from './Footer/Footer'
 import Navbar from './Navbar/Navbar'
 
 const Template = () => {
