5788c94d1d1737fc7aed6e1617160ed36f67809e
Adita Kumar <aditya22033@iiitd.ac.in>
2025-01-19 11:55:18 +0530

    Guide page

---
+++ src/Pages/Guide.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Guide.css';
+
+const Guide = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="left-section"></div>
+        <div class="right-section">
+          <div class='picture'></div>
+          <div class='info'></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Guide;
