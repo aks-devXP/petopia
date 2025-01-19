feeb4ede22b9cb5d046f2bb7e5eeaef14dfd84d3
akmorningstar <akash22048@iiitd.ac.in>
2025-01-20 00:19:39 +0530

    Smooth Scrolling Feature Implemented
    Using Lenis and Gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import MainLayout from './Layout/MainLayout'
 import About from './Pages/About'
-import Blog from "./pages/Blog"
 import Contact from './Pages/Contact'
 import Home from './pages/Home'
 import Login from './Pages/Login'
+import News from './pages/News'
+import NewsPage from './pages/NewsPage'
 import Signup from './Pages/Signup'
 import VetHome from "./pages/VetHome"
 import Volunteer from "./pages/Volunteer"
-import News from './pages/News'
-import NewsPage from './pages/NewsPage'
 
+// For smooth scrolling
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Lenis from 'lenis'
+import { useEffect } from 'react'
+
+gsap.registerPlugin(ScrollTrigger);
 function App() {
+
+  useEffect(() => {
+    // Initialize Lenis for smooth scrolling
+    const lenis = new Lenis();
+
+    // Synchronize Lenis scrolling with GSAP's ScrollTrigger
+    lenis.on('scroll', ScrollTrigger.update);
+
+    // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+    });
+
+    // Disable GSAP's lag smoothing
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove((time) => {
+        lenis.raf(time * 1000);
+      });
+      lenis.destroy(); // Cleanup on unmount
+    };
+  }, []);
+
     const router = createBrowserRouter(createRoutesFromElements(
       <>
       <Route path='/login' element={<Login/>}></Route>
