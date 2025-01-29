e94adc535d8cff137bba8fec47ce8fa8132cd89f
Adita Kumar <aditya22033@iiitd.ac.in>
2025-01-29 23:49:14 +0530

    svg + navbar + guide page

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,96 +1,121 @@
-import React, { useState } from 'react';
-import { RiCloseLine, RiMenu3Fill } from 'react-icons/ri';
-import { NavLink } from 'react-router-dom';
-import logo from '../assets/petopia-logo-color.png';
-import './Navbar.css';
+import React, { useState } from "react";
+import { RiCloseLine, RiMenu3Fill } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/petopia-logo.svg";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [loggedin, setLoggedin] = useState(false);
   const [toggle, setToggle] = useState(false);
-  const menuOff = ()=>{
+  const userName = "UserName"; // Replace with a dynamic value if needed
+
+  const handleSignInLogIn = () => {
+    setLoggedin(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedin(false);
+  };
+
+  const menuOff = () => {
     setToggle(false);
     console.log("menu off");
   };
-  const [loggedin, setLoggedin] = useState("Sign in");
 
-
-  const Submenu = ()=>{
+  const Submenu = () => {
     return (
       <ul>
         <li>
-          <NavLink to={"/vet"} >Veterinary</NavLink>
+          <NavLink to="/news">News</NavLink>
         </li>
         <li>
-          <NavLink to={"/ngo"} >NGO</NavLink>
+          <NavLink to="/ngo">NGO</NavLink>
         </li>
         <li>
-          <NavLink to={"/shopping"} >Products</NavLink>
+          <NavLink to="/about">About Us</NavLink>
         </li>
         <li>
-          <NavLink to={"/trainers"} >Training</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
-    )
-  }
+    );
+  };
 
-  const Menu = ()=>{
-    return(
-      
-        <ul>
-          <li>
-            <NavLink to={"/home"} >Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/about"} >About</NavLink>
-          </li>
-          <li className='submenu'>
-            <NavLink to={"#"} >Services</NavLink>
-            <Submenu/>
-          </li>
-          <li>
-            <NavLink to={"/contact"} >Contact</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/news"} >News</NavLink>
-          </li>
-        </ul>
-      
-    )
-  }
+  const Menu = () => {
+    return (
+      <ul>
+        <li>
+          <NavLink to="/home">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/guide">Guide</NavLink>
+        </li>
+        <li>
+          <NavLink to="/vet">Medical Care</NavLink>
+        </li>
+        <li>
+          <NavLink to="/shopping">Pet Essentials</NavLink>
+        </li>
+        <li>
+          <NavLink to="/trainers">Trainer</NavLink>
+        </li>
+        <li className="submenu">
+          <NavLink to="#">More</NavLink>
+          <Submenu />
+        </li>
+      </ul>
+    );
+  };
+
   return (
     <div className="navbar">
       <div className="navbar-links">
         <div className="navbar-links_logo">
-          <img src={logo} alt="" />
+          <NavLink to="/home">
+            <img src={logo} alt="Petopia Logo" />
+          </NavLink>
         </div>
         <div className="navbar-links_container">
-          <Menu/>
+          <Menu />
         </div>
       </div>
+
       <div className="navbar-sign">
-        <button type="button" onClick={()=>{
-          if(loggedin === "Sign in"){
-            setLoggedin("Sign out");
-          }else{
-            setLoggedin("Sign in");
-          }
-        }}>{loggedin}</button>
+        {loggedin ? (
+          <button className="user-button" type="user-button" onClick={handleLogout}>
+            Hi, {userName}
+          </button>
+        ) : (
+          <>
+            <button className="sign-in-button" type="sign-in-button" onClick={handleSignInLogIn}>
+              Sign in
+            </button>
+            <button className="log-in-button" type="log-in-button" onClick={handleSignInLogIn}>
+              Log in
+            </button>
+          </>
+        )}
       </div>
-      
-      <div className="navbar-menu" onBlur= {menuOff}>
-        {toggle?<RiCloseLine color="#fff" onClick={()=>setToggle(false)}/>:<RiMenu3Fill color="#fff" onClick={()=>setToggle(true)}/>}
-          {
-            toggle && (<div className="navbar-menu_container scale-up-center" >
-              <div className="navbar-menu_container-links">
-                <Menu/>
-                  <div className="navbar-menu_container-links-sign">
-                  <button type="button">Sign up</button>
-                </div>
+
+      <div className="navbar-menu" onBlur={menuOff}>
+        {toggle ? (
+          <RiCloseLine color="#fff" onClick={() => setToggle(false)} />
+        ) : (
+          <RiMenu3Fill color="#fff" onClick={() => setToggle(true)} />
+        )}
+        {toggle && (
+          <div className="navbar-menu_container scale-up-center">
+            <div className="navbar-menu_container-links">
+              <Menu />
+              <div className="navbar-menu_container-links-sign">
+                <button type="button">Sign up</button>
               </div>
-              </div> )
-          }
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
