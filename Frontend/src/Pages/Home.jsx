fa22a22e211bd5f445cb5b20531c7af77afee078
Adita Kumar <aditya22033@iiitd.ac.in>
2025-03-07 01:27:45 +0530

    appointment page create

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Slider from '../Components/Slider'
 import HeroNav from '../Components/HeroNav'
+import ScrollAnimations from '../Components/ScrollAnimations'
 const Home = () => {
   return (
     <div className='my-2 bg-slate-500'>
-      <Slider></Slider>
-      <HeroNav></HeroNav>
+      <ScrollAnimations/>
     </div>
   )
 }
