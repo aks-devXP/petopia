f124c28f267612305b2c380fa5dfc08b844c341a
Akanksh <akanksh22046@iiitd.ac.in>
2025-03-10 03:43:38 +0530

    Revert "VetBackend Implemented"
    This reverts commit 20a95b4694818610e1aeb37a4191fb1163394c54.

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -3,10 +3,8 @@ const router = exp.Router();
 const { ContactControl } = require('../Controllers/user');
 const {ContactValidation} = require('../Middlewares/user');
 const {UserValidation} = require('../Middlewares/user');
-const {getProfileControl,updateProfileControl, updatePasswordControl} = require('../Controllers/user');
+const {getProfileControl} = require('../Controllers/user');
 router.post('/contact-us', ContactValidation, ContactControl);
 
 router.get('/profile-info', UserValidation, getProfileControl);
 module.exports = router;
-router.post('/profile-info', UserValidation, updateProfileControl);
-router.put('/profile-pass', UserValidation, updatePasswordControl);
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Slider from '../Components/Slider'
+import HeroNav from '../Components/HeroNav'
+import ScrollAnimations from '../Components/ScrollAnimations'
 const Home = () => {
   return (
     <div className='my-2 bg-slate-500'>
-      {/* <ScrollAnimations/> */}
+      <ScrollAnimations/>
     </div>
   )
 }
