e83ff6ea88169f8782dc904dc6cce16baaff0481
Akanksh <akanksh22046@iiitd.ac.in>
2025-03-20 01:25:41 +0530

    Changed Navbar, Improved Vet Booking & Profile Dropdown

--- Frontend/src/Components/NavbarMenu/MainMenu.jsx
+++ Frontend/src/Components/NavbarMenu/MainMenu.jsx
@@ -1,42 +1,36 @@
+import { Bone, BookOpenTextIcon, ChevronDown, Home, MoveRight, PawPrint, Store } from 'lucide-react';
 import { NavLink } from "react-router-dom";
-import { PersonStanding, BriefcaseMedical, Store, House } from 'lucide-react';
-
 
 const Submenu = () => {
   return (
     <ul className="absolute top-full left-[-2rem] bg-[#151516] rounded-md p-2 hidden group-hover:block z-50">
-      <li className="mt-2">
-        <NavLink to="/news" className="flex justify-between items-center p-2 min-w-[15rem] rounded-md hover:bg-[#1f1d1d] hover:text-[#E5E5CB] hover:p-4">
-          News
-          <span className="opacity-0 hover:opacity-100">
-            <img src="../assets/icon-arrow-right.svg" className="h-[17px]" />
-          </span>
-        </NavLink>
-      </li>
-      <li className="mt-2">
-        <NavLink to="/ngo" className="flex justify-between items-center p-2 min-w-[15rem] rounded-md hover:bg-[#1f1d1d] hover:text-[#E5E5CB] hover:p-4">
-          NGO
-          <span className="opacity-0 hover:opacity-100">
-            <img src="../assets/icon-arrow-right.svg" className="h-[17px]" />
-          </span>
-        </NavLink>
-      </li>
-      <li className="mt-2">
-        <NavLink to="/about" className="flex justify-between items-center p-2 min-w-[15rem] rounded-md hover:bg-[#1f1d1d] hover:text-[#E5E5CB] hover:p-4">
-          About Us
-          <span className="opacity-0 hover:opacity-100">
-            <img src="../assets/icon-arrow-right.svg" className="h-[17px]" />
-          </span>
-        </NavLink>
-      </li>
-      <li className="mt-2">
-        <NavLink to="/contact" className="flex justify-between items-center p-2 min-w-[15rem] rounded-md hover:bg-[#1f1d1d] hover:text-[#E5E5CB] hover:p-4">
-          Contact
-          <span className="opacity-0 hover:opacity-100">
-            <img src="../assets/icon-arrow-right.svg" className="h-[17px]" />
-          </span>
-        </NavLink>
-      </li>
+      {[
+        { to: "/news", label: "News" },
+        { to: "/ngo", label: "NGO" },
+        { to: "/about", label: "About Us" },
+        { to: "/contact", label: "Contact" }
+      ].map((item, index) => (
+        <li key={index} className="mt-2 group/item">
+          <NavLink
+            to={item.to}
+            className="relative flex justify-between items-center p-2 min-w-[15rem] rounded-md 
+              group-hover/item:bg-[#1f1d1d] group-hover/item:text-[#E5E5CB] 
+              transition-all duration-300 
+              group-hover/item:pl-4 group-hover/item:pr-4"
+          >
+            {item.label}
+            <div className="relative w-5 h-5 ml-2">
+              <MoveRight 
+                className="absolute top-1/2 left-1/2 
+                  -translate-x-[150%] -translate-y-1/2 
+                  opacity-0 w-4 h-4
+                  transition-all duration-300 
+                  group-hover/item:opacity-100 group-hover/item:translate-x-[-50%]"
+              />
+            </div>
+          </NavLink>
+        </li>
+      ))}
     </ul>
   );
 };
@@ -45,43 +39,85 @@ const Menu = () => {
   return (
     <ul className="list-none p-0 m-0 flex gap-12 h-15 items-center">
       <li>
-        <NavLink to="/home" className = {({ isActive }) => `text-white font-medium text-lg leading-[25px] capitalize mx-4 cursor-pointer hover:text-[#E5E5CB] ${isActive ? "border-2 rounded-3xl" : ""}`}>
-          <div className= {`flex justify-center transition-transform`}>
-            <div className="mr-2">
-              <House/>
+        <NavLink to="/home" className="text-white font-medium text-lg leading-[25px] capitalize mx-4 cursor-pointer hover:text-[#E5E5CB]">
+          {({ isActive }) => (
+            <div className={`flex justify-center transition-transform ${isActive ? "border-2 border-white rounded-3xl p-2" : ""}`}>
+              { isActive?(<div className="px-3">
+                <Home />
+              </div>):
+              (<p>Home</p>)
+              }
             </div>
-            <p>Home</p>
-          </div>
+          )}
         </NavLink>
       </li>
       <li>
         <NavLink to="/dictionary" className="text-white font-medium text-xl leading-[25px] capitalize mx-4 cursor-pointer hover:text-[#E5E5CB]">
-          Guide
+          {({ isActive }) => (
+            <div className={`${isActive ? "border-2 border-white rounded-3xl p-2" : ""}`}>
+              { isActive?(<div className="px-3">
+                <BookOpenTextIcon />
+              </div>):
+              (<p>Guide</p>)
+              }
+            </div>
+          )}
         </NavLink>
       </li>
       <li>
         <NavLink to="/vet" className="text-white font-medium text-xl leading-[25px] capitalize mx-4 cursor-pointer hover:text-[#E5E5CB]">
-          Medical Care
+          {({ isActive }) => (
+            <div className={`${isActive ? "border-2 border-white rounded-3xl p-2" : ""}`}>
+              { isActive?(<div className="px-3">
+                <PawPrint />
+              </div>):
+              (<p>Medic</p>)
+              }
+            </div>
+          )}
         </NavLink>
       </li>
       <li>
         <NavLink to="/shopping" className="text-white font-medium text-xl leading-[25px] capitalize mx-4 cursor-pointer hover:text-[#E5E5CB]">
-          Pet Essentials
+          {({ isActive }) => (
+            <div className={`${isActive ? "border-2 border-white rounded-3xl p-2" : ""}`}>
+              { isActive?(<div className="px-3">
+                <Store />
+              </div>):
+              (<p>Pet Essentials</p>)
+              }
+            </div>
+          )}
         </NavLink>
       </li>
       <li>
         <NavLink to="/trainer" className="text-white font-medium text-xl leading-[25px] capitalize mx-4 cursor-pointer hover:text-[#E5E5CB]">
-          Trainer
-        </NavLink>
+            {({ isActive }) => (
+              <div className={`${isActive ? "border-2 border-white rounded-3xl p-2" : ""}`}>
+                { isActive?(<div className="px-3">
+                  <Bone />
+                </div>):
+                (<p>Trainer</p>)
+                }
+              </div>
+            )}
+          </NavLink>
       </li>
-      <li className="relative flex gap-2 items-center justify-start group">
-        <NavLink to="#" className="text-white font-medium text-xl leading-[25px] capitalize mx-4 cursor-pointer group-hover:text-[#E5E5CB] after:content-[''] after:w-6 after:h-6 after:opacity-50 after:bg-[url('../assets/icon-arrow-down.svg')] after:bg-contain after:bg-no-repeat">
-          More
+      <li className="relative group">
+        <NavLink 
+          to="/bc" 
+          className="text-white font-medium text-xl leading-[25px] capitalize mx-4 cursor-pointer group-hover:text-[#E5E5CB]"
+        >
+          {({ isActive }) => (
+            <div className={`flex items-center ${isActive ? "border-2 border-white rounded-3xl p-2" : ""}`}>
+              More
+              <ChevronDown className="ml-2 w-5 h-5 opacity-50 group-hover:opacity-100" />
+            </div>
+          )}
         </NavLink>
         <Submenu />
       </li>
     </ul>
   );
 };
-
 export default Menu;
--- Frontend/src/Components/Vet/DateTile.jsx
+++ Frontend/src/Components/Vet/DateTile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DateTile = ({day,date,toggle,click}) => {
   return (
     <>
-        <div className={`flex flex-col items-center border-2 rounded-full w-16 h-20 justify-center shadow-md cursor-pointer hover:bg-n-12 ${toggle ? 'bg-n-12' : 'bg-inherit'}`} onClick={click}>
+        <div className={`flex flex-col items-center border-2 rounded-3xl w-fit h-fit px-6 py-2 justify-center shadow-md cursor-pointer hover:bg-n-12 ${toggle ? 'bg-n-12' : 'bg-inherit'} duration-500`} onClick={click}>
             <span>{day}</span>
             <span>{date}</span>
         </div>
