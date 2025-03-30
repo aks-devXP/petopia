276b50deff7994c1f6b673f85fb967db8d36a3f3
Akanksh <akanksh22046@iiitd.ac.in>
2025-03-31 02:52:32 +0530

    Added basicHero Component

--- Frontend/src/Pages/NGO.jsx
+++ Frontend/src/Pages/NGO.jsx
@@ -4,11 +4,11 @@ import Contribute from '../Components/NGO comp/Contribute'
 import LayeredCard from '../Components/NGO comp/LayeredCard'
 const NGO = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-8'>
+    <div className='w-full'>
      <div className='w-full'>
       <Banner/>
      </div>
-     <div>
+     <div className='w-full'>
       <Contribute/>
      </div>
       <div>
