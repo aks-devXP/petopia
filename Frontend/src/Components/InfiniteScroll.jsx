8de88243244525b1b7b33bac6734914b3516ce68
Akanksh <akanksh22046@iiitd.ac.in>
2025-03-31 01:02:56 +0530

    fixed infinite scroll images

--- Frontend/src/Components/InfiniteScroll.jsx
+++ Frontend/src/Components/InfiniteScroll.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
-import { motion } from "framer-motion";
 import Marquee from "react-fast-marquee";
-import delhi from "../assets/Vet/delhi.jpg"
-
+import { getImgUrl } from '../Util/ImageExtract';
 
 const InfiniteScroll = ({text,data}) => {
 
+    const path = "../assets/Vet/"
+
     const logos = [
-        { name: "Delhi", src: "../assets/Vet/delhi.jpg" },
-      ];      
+        { name: "Delhi", src: `${path}delhi.jpg`},
+        { name: "Ahmedabad", src: `${path}ahmedabad.jpg`},
+        { name: "Hyderabad", src: `${path}hyderabad.jpg`},
+        { name: "Mumbai", src: `${path}mumbai.jpg`},
+        { name: "Pune", src: `${path}pune.jpg`},
+        { name: "Srinagar", src: `${path}Srinagar.jpg`},
+    ];
 
   return (
     <>
         <div className="bg-[#0f2747] w-full overflow-hidden py-8">
-            <div className="max-w-7xl mx-auto flex ">
+            <div className="max-w-7xl mx-auto flex items-center">
                 <h3 className="text-white text-xl max-w-36 font-semibold mb-5">
                     {text ? text : "Award winning & featured in..."}
                 </h3>
-                <div className="relative w-full overflow-hidden">
+                <div className="relative w-full overflow-hidden ">
                     <Marquee className='flex gap-5' direction='left'>
                         {data ? {
 
@@ -25,9 +30,9 @@ const InfiniteScroll = ({text,data}) => {
                             [...logos, ...logos].map((logo, index) => (
                             <img
                                 key={index}
-                                src="../assets/Vet/delhi.jpg"
+                                src={getImgUrl(logo.src)}
                                 alt={logo.name}
-                                className="h-10 w-auto object-contain mx-10"
+                                className="h-32 rounded-3xl w-auto object-contain mx-5"
                             />
                             ))
                         }
