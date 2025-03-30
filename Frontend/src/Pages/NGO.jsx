1e30065b02386f9e62024b1c28bb9fb69fba43be
akmorningstar <akash22048@iiitd.ac.in>
2025-03-31 00:57:11 +0530

    NGO page Half implemented

--- Frontend/src/Pages/NGO.jsx
+++ Frontend/src/Pages/NGO.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
+import Banner from '../Components/NGO comp/Banner'
+import Contribute from '../Components/NGO comp/Contribute'
 import LayeredCard from '../Components/NGO comp/LayeredCard'
-import StackedCards from '../Components/NGO comp/StackedCards'
-
 const NGO = () => {
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <h1>
-        Welcome to Our NGO 
-      </h1>
-      <div className='h-1/3'>
-      <StackedCards/>
-      </div>
-      
+    <div className='flex flex-col items-center justify-center gap-8'>
+     <div className='w-full'>
+      <Banner/>
+     </div>
+     <div>
+      <Contribute/>
+     </div>
       <div>
         <LayeredCard/>
       </div>
