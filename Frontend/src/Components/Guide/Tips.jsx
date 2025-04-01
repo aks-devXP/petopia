b756c3bc4bcfa94e2aaeb2b894f1a4886d69d099
Adita Kumar <aditya22033@iiitd.ac.in>
2025-04-02 00:05:41 +0530

    guide page complete

--- Frontend/src/Components/Guide/Tips.jsx
+++ Frontend/src/Components/Guide/Tips.jsx
@@ -20,67 +20,64 @@ export default function Tips({petSelected, setPetSelected}) {
   return (
     <div className={`h-full w-full ${petSelected === "Dogs" ? "bg-amber-100" : "bg-purple-200"} min-h-screen flex`}>
 
-<div className="h-full w-1/5 flex flex-col justify-between items-center text-black/70 font-bold text-xl py-10">
-    {/* Training */}
-    <div className="relative w-full h-full flex justify-center items-center">
-      <div className="absolute inset-0 z-0">
-        <SvgBlob1 />
-      </div>
-      <div
-        onClick={() => handleSelect("Training")}
-        className={`z-10 relative flex flex-col items-center justify-center w-full h-full 
-          ${tipSelected === "Training" ? "scale-125 text-black" : "hover:scale-110"} transition-transform duration-300 ease-in-out hover:cursor-pointer`}
-      >
-        <img src={Training} alt="" className="max-w-[40%] max-h-[40%] object-contain" />
-        <span>Training</span>
-      </div>
-    </div>
+      <div className="h-full w-1/5 flex flex-col justify-between items-center text-black/70 font-bold text-xl py-10">
+          {/* Training */}
+          <div className="relative w-full h-full flex justify-center items-center">
+            <div className="absolute inset-0 z-0">
+              <SvgBlob1 />
+            </div>
+            <div
+              onClick={() => handleSelect("Training")}
+              className={`z-10 relative flex flex-col items-center justify-center w-full h-full 
+                ${tipSelected === "Training" ? "scale-125 text-black" : "hover:scale-110"} transition-transform duration-300 ease-in-out hover:cursor-pointer`}
+            >
+              <img src={Training} alt="" className="max-w-[40%] max-h-[40%] object-contain" />
+              <span>Training</span>
+            </div>
+          </div>
 
-    {/* Grooming */}
-    <div className="relative w-full h-full flex justify-center items-center">
-      <div className="absolute inset-0 z-0">
-        <SvgBlob2 />
-      </div>
-      <div
-        onClick={() => handleSelect("Grooming")}
-        className={`z-10 relative flex flex-col items-center justify-center w-full h-full 
-          ${tipSelected === "Grooming" ? "scale-125 text-black" : "hover:scale-110"} transition-transform duration-300 ease-in-out hover:cursor-pointer`}
-      >
-        <img src={Grooming} alt="Your Image" className="max-w-[40%] max-h-[40%] object-contain" />
-        <span>Grooming</span>
-      </div>
-    </div>
+          {/* Grooming */}
+          <div className="relative w-full h-full flex justify-center items-center">
+            <div className="absolute inset-0 z-0">
+              <SvgBlob2 />
+            </div>
+            <div
+              onClick={() => handleSelect("Grooming")}
+              className={`z-10 relative flex flex-col items-center justify-center w-full h-full 
+                ${tipSelected === "Grooming" ? "scale-125 text-black" : "hover:scale-110"} transition-transform duration-300 ease-in-out hover:cursor-pointer`}
+            >
+              <img src={Grooming} alt="Your Image" className="max-w-[40%] max-h-[40%] object-contain" />
+              <span>Grooming</span>
+            </div>
+          </div>
 
-    {/* Feeding */}
-    <div className="relative w-full h-full flex justify-center items-center">
-      <div className="absolute inset-0 z-0">
-        <SvgBlob3 />
+          {/* Feeding */}
+          <div className="relative w-full h-full flex justify-center items-center">
+            <div className="absolute inset-0 z-0">
+              <SvgBlob3 />
+            </div>
+            <div
+              onClick={() => handleSelect("Feeding")}
+              className={`z-10 relative flex flex-col items-center justify-center w-full h-full 
+                ${tipSelected === "Feeding" ? "scale-125 text-black" : "hover:scale-110"} transition-transform duration-300 ease-in-out hover:cursor-pointer`}
+            >
+              <img src={Feeding} alt="Your Image" className="max-w-[40%] max-h-[40%] object-contain" />
+              <span>Feeding</span>
+            </div>
+          </div>
       </div>
-      <div
-        onClick={() => handleSelect("Feeding")}
-        className={`z-10 relative flex flex-col items-center justify-center w-full h-full 
-          ${tipSelected === "Feeding" ? "scale-125 text-black" : "hover:scale-110"} transition-transform duration-300 ease-in-out hover:cursor-pointer`}
-      >
-        <img src={Feeding} alt="Your Image" className="max-w-[40%] max-h-[40%] object-contain" />
-        <span>Feeding</span>
-      </div>
-    </div>
-  </div>
 
-      <div className="w-3/5 flex justify-center items-center">
+      <div className="w-3/5 flex justify-center items-center ">
         <PetCareTips petSelected={petSelected} tipSelected={tipSelected}/>
       </div>
 
       <div className="flex-1 flex items-center justify-center relative flex-col">
-
           <PetToggle setPetSelected={setPetSelected} petSelected={petSelected}/>
-
           <img 
             src={petSelected === "Dogs" ? Doggy : Cat}
-            alt="" 
+            alt={petSelected === "Dogs" ? "Dog" : "Cat"}
             className="max-w-full h-auto rounded-lg object-cover m-4"
           />
-          
         </div>
       </div>
   )
