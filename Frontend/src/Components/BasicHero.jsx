7352b096b93dd0b0b235a345ae75e8ec65f3ea02
Akanksh <akanksh22046@iiitd.ac.in>
2025-04-03 11:24:33 +0530

    Completed NGO Frontend

--- Frontend/src/Components/BasicHero.jsx
+++ Frontend/src/Components/BasicHero.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const BasicHero = ({ imageSrc, title, description, buttonText, imageLeft, bgColor }) => {
+const BasicHero = ({ imageSrc, title, description, buttonText, imageLeft, bgColor, textColor, textColor2, to }) => {
+  const navigate = useNavigate();
   return (
-    <div className={`${bgColor ? `bg-[${bgColor}]` : "bg-[#C2F7F0]"} h-full flex flex-col md:flex-row items-center justify-between gap-10 p-10 rounded-lg`}>
+    <div className={`${bgColor ? bgColor : "bg-[#C2F7F0]"} h-full flex flex-col md:flex-row items-center justify-between gap-10 p-10 rounded-lg`}>
       {/* Content on the left or right depending on imageLeft */}
 
       {!imageLeft && (
         <div className="md:w-1/2 space-y-4">
-          <h2 className="text-4xl font-bold text-gray-900">{title}</h2>
-          <p className="text-gray-700 text-lg">{description}</p>
-          {/* <button className="bg-black text-white px-6 py-2 rounded-full hover:bg-gray-800">
+          <h2 className={`text-4xl font-bold ${textColor ? textColor : "text-gray-900"} `}>{title ? title : "Add title Here"}</h2>
+          <p className={`${textColor2 ? textColor2 : "text-gray-700"} text-lg`}>{description}</p>
+          <button onClick={()=>navigate({to})} className="bg-black text-white px-6 py-2 rounded-xl hover:bg-gray-800">
             {buttonText}
-          </button> */}
+          </button>
         </div>
       )}
 
@@ -29,11 +31,11 @@ const BasicHero = ({ imageSrc, title, description, buttonText, imageLeft, bgColo
       {/* Content for imageLeft */}
       {imageLeft && (
         <div className="md:w-1/2 space-y-4">
-          <h2 className="text-4xl font-bold text-gray-900">{title}</h2>
-          <p className="text-gray-700 text-lg">{description}</p>
-          {/* <button className="bg-black text-white px-6 py-2 rounded-full hover:bg-gray-800">
+            <h2 className={`text-4xl font-bold ${textColor ? textColor : "text-gray-900"} `}>{title ? title : "Add title Here"}</h2>
+          <p className={`${textColor2 ? textColor2 : "text-gray-700"} text-lg`}>{description}</p>
+          <button onClick={()=>navigate(to)} className="bg-black text-white px-6 py-2 rounded-xl hover:bg-gray-800">
             {buttonText}
-          </button> */}
+          </button>
         </div>
       )}
     </div>
